a9321c2ce991defc63185e18eccc187be8dc1a15
Tim Dettinger <6506703@isd535.org>
2022-03-17 15:22:54 -0500

    desciptions and formatting

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,29 @@
 
 const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_DESC_SELECTOR = '[data-image-role="desc"]';
 const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 const HIDDEN_DETAIL_CLASS = 'hidden-deatil';
 const TINY_EFFECT_CLASS = 'is-tiny';
 const ESC_KEY_CODE = 27;
+const TEXT = ['Unicef provides over 600 million meals to those in need each year! Making sure mouths are fed is the first step to giving hope to those in need.',
+ 'Giving children the ability to be happy and curious is crutial to helping them develop. Unicef sends thousands of staff and voluneeters to play with and cheer up the children.',
+  'Unicef provides those in poverty with the tools they need to work their way out. School supplies are crutial to the education of the youth and Unicef is proud to help.',
+   'Unicef trained staff help children learn where they otherwise would not be able to. Education is key to creating a better future and enabling the potential of these humans.',
+    'Respect is huge at Unicef. Everyone is born human and where you come from is not your choice, so everyone is treated with dignity and respect. Unicef is proud to uplift those who are struggling in life.'];
 
-function setDetails(imageUrl, titleText) {
+function setDetails(imageUrl, titleText, titleDescription) {
     'use strict'
     let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR)
     detailImage.setAttribute('src' , imageUrl);
 
     let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
     detailTitle.textContent = titleText;
-}
+    
+    let detailDesc = document.querySelector(DETAIL_DESC_SELECTOR);
+    detailDesc.textContent = titleDescription;
+ }
 
 function imageFromThumb(thumbnail) {
     'use strict';
@@ -28,7 +37,13 @@ function titleFromThumb(thumbnail) {
 
 function setDetailsFromThumb(thumbnail) {
     'use strict';
-    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail), textFromThumb(thumbnail));
+}
+
+function textFromThumb(thumbnail) {
+    'use strict';
+    let index = parseInt(thumbnail.getAttribute('link-index'));
+    return TEXT[index];
 }
 
 function addThumbClickHandler(thumb) {
